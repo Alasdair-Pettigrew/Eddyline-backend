59b0d3ec302eca5ba4bfd5a4ce9cccfb6d08e452
Alasdair-Pettigrew <alasdair@eddylineventures.com>
2024-06-26 21:16:41 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+const express = require('express');
+const dotenv=require('dotenv');
+
+const cors = require('cors'); 
+// ...
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+app.use(bodyParser.json());
+// Serve static files from the 'Front-end' folder
+
+
+
+mongoose.connect(process.env.api_key, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const contactSchema = new mongoose.Schema ({
+    name: String,
+    email: String,
+    message: String,
+});
+const Contact = mongoose.model('Contact', contactSchema);
+
+
+
+app.post('/add', (req, res) => {
+    const newContact = new Contact({
+        name: req.body.name,
+        email: req.body.email,
+        message: req.body.message,
+    });
+    newContact.save()
+    .then(() => {
+        res.json({ message: 'Data submitted successfully!' });
+    })
+    .catch(error => {
+        console.error('Error saving data:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    });
+  
+});
+
+// Add a route to serve the CSS file
+
+app.listen(3000, function () {
+    console.log('App is running on http://localhost:3000');
+});
